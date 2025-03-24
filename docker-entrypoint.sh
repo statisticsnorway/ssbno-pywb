fc1034dcb242328272dd3880d6d3f3369183b472
Carl-OW <142233642+Carl-OW@users.noreply.github.com>
2025-03-24 14:15:37 +0100

    back to normal

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,18 +1,8 @@
 #!/bin/sh
-set -e
 
+# Changed to avoid using sudo 
+set -e
 if [ -n "$INIT_COLLECTION" ] && [ ! -d "$VOLUME_DIR/collections/$INIT_COLLECTION" ]; then
     wb-manager init "$INIT_COLLECTION" || echo "Warning: Could not initialize collection."
 fi
-
-INDEX_DIR="$VOLUME_DIR/collections/$INIT_COLLECTION/indexes"
-ARCHIVE_DIR="$VOLUME_DIR/collections/$INIT_COLLECTION/archive"
-
-if [ -z "$(ls -A "$INDEX_DIR" 2>/dev/null)" ]; then
-    echo "No indexes found, running manual indexing of archive..."
-    wayback index --no-append --output-dir "$INDEX_DIR" "$ARCHIVE_DIR"/*.warc.gz || echo "Warning: Indexing failed or no WARCs found."
-else
-    echo "Indexes already present, skipping initial indexing."
-fi
-
 exec "$@"
